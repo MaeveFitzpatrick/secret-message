cdd4709af1dfa8428e6c14be6b2527f4cc2dbace
MaeveFitzpatrick <mfitz004@gmail.com>
2022-05-13 13:35:13 -0700

    created prompts

--- src/secretMessage/Main.java
+++ src/secretMessage/Main.java
@@ -7,9 +7,13 @@ public class Main {
 	public static void main (String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Hello! Would you like to encode or decode a message?");
-		String codeResponse = input.next();
+		String codeResponse = input.nextLine();
+		System.out.println("What is the name of the input file?");
+		String inputFileName = input.nextLine();
+		System.out.println("What is the name of output file?");
+		String outputFileName = input.nextLine();
 	}
-	public static boolean processFile(String key, File inputFileName, File outputFileName, File encodeDecode) {
+	public static boolean processFile(String key, String encodeDecode, String inputFileName, String outputFileName) {
 		return true;
 	}
 
