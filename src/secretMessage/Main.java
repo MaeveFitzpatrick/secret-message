4744a877a32087ef3575ac31979091f4685ffd15
MaeveFitzpatrick <mfitz004@gmail.com>
2022-05-13 13:02:32 -0700

    created Scanner and codeResponse input

--- src/secretMessage/Main.java
+++ src/secretMessage/Main.java
@@ -1,9 +1,12 @@
 package secretMessage;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main (String[] args) {
-		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Hello! Would you like to encode or decode a message?");
+		String codeResponse = input.next();
 	}
 
 }
